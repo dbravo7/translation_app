e47aa6b398a1151a742c0fc2ba1f2fdee6aae0c1
David <dbravo7@yahoo.com>
2019-06-26 14:17:43 -0400

    Added media queries for MVP; blocked out duo translate working feature

--- index.js
+++ index.js
@@ -1,64 +1,94 @@
 "use strict";
 
-function duoCompare() {
-  $('#js-compare').click(() => {
+// function duoCompare() {
+//   $('#js-compare').click(() => {
 
-    let google_translation = $('#js-google-translate').text();
-    let yandex_translation = $('#js-yandex-translate').text();
-    const target_code = $('#js-target-languages').val();
+//     let google_translation = $('#js-google-translate').text();
+//     let yandex_translation = $('#js-yandex-translate').text();
+//     const target_code = $('#js-target-languages').val();
 
-    if (target_code === "en") {
-      let google_arr = google_translation.split(" ");
-      let yandex_arr = yandex_translation.split(" ");
-      const indices = compareTranslations(yandex_arr, google_arr);
-      updateGoogleTranslation(indices, google_arr);
-      updateYandexTranslation(indices, yandex_arr);
-     } else {
-      alert('This feature is currently only available for translations to English');
-     }
-  });
-}
+//     if (target_code === "en") {
+//       let google_arr = google_translation.split(" ");
+//       let yandex_arr = yandex_translation.split(" ");
+//       const indices = compareTranslations(yandex_arr, google_arr);
+//       let google_update = updateTranslation(indices, google_arr);
+//       let yandex_update = updateTranslation(indices, yandex_arr);
+//       displayComparedTranslations(google_update, yandex_update);
+//      } else {
+//       alert('This feature is currently only available for translations to English');
+//      }
+//   });
+// }
 
-function compareTranslations(yandex_arr, google_arr) {
-  let indices = [];
+// function compareTranslations(yandex_arr, google_arr) {
+//   let indices = [];
+  // if google_arr.length !== yandex_arr.length {
+  // let longer_text = longerTranslation(google_arr, yandex_arr);
+  // let shorter_text = shorterTranslation
+  // }
 
-  google_arr.forEach(function (element, index, google_arr) {
-    if (google_arr[index].toLowerCase() !== yandex_arr[index].toLowerCase()) {
-      indices.push(index);
-    }
-  });
-  return indices;
-}
+//   google_arr.forEach(function (element, index, google_arr) {
+//     if ((google_arr[index] && yandex_arr[index]) &&
+//      (google_arr[index].toLowerCase() !== yandex_arr[index].toLowerCase())) {
+//       indices.push(index);
+//     } else {
+//       indices.push(index); 
+//     }
+//   });
+//   return indices;
+// }
+
+// function updateTranslation(indices, translation_arr) {
+//   let new_arr = [];
+  
+//     translation_arr.forEach(function(ele, index) {
+//       if (index === indices[index]) {
+//         new_arr.push(`<span class="highlight">${ele}</span>`);
+//       } else {
+//         new_arr.push(ele);
+//       }
+//   });
+//   return new_arr.join(" ");
+// }
+
+// function displayComparedTranslations(google_update, yandex_update) {
+//   $('#js-google-translate').html(`${google_update}`);
+//   $('#js-display-input').html(`${yandex_update}`); 
+// }
 
 function handleSubmitText() {
   $('#js-submit').click(event => {
     event.preventDefault();
     const text = $('#js-input-text').val();
     const target_code = $('#js-target-languages').val();
-  
-    // Gets language 2-3 letter code and displays it as 'auto detect'
-    getSourceCode(text); 
-    
-    // Gets translated texts from respective APIs and displays them
-    getGoogleTranslate(text);
-    getYandexTranslate(text, target_code);
+
+    if (text) {
+      // Gets translated texts from respective APIs and displays them
+      getGoogleTranslate(text);
+      getYandexTranslate(text, target_code);
+    }
   });
 }
 
-function getSourceCode(text) {
-  const key = config.Y_KEY; 
-  const url = `https://translate.yandex.net/api/v1.5/tr.json/detect?key=${key}&text=${text}`;
+function getSourceCode() {
+  $('#js-input-text').change(event => {
+    let text = $('#js-input-text').val();
+    if (text) {
 
-  fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(response.statusText);
-    })
-    .then(responseJson => displaySourceLang(responseJson.lang))
-    .catch(error => {
-      alert(`Something went wrong: ${error.message}`);
+      const key = config.Y_KEY; 
+      const url = `https://translate.yandex.net/api/v1.5/tr.json/detect?key=${key}&text=${text}`;
+      fetch(url)
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error(response.statusText);
+        })
+        .then(responseJson => displaySourceLang(responseJson.lang))
+        .catch(error => {
+          alert(`Something went wrong: ${error.message}`);
+      });
+    } 
   });
 }
 
@@ -81,7 +111,7 @@ function getGoogleTranslate(text, lang='en') {
     })
     .then(responseJson => displayGoogleTranslate(responseJson.data.translations[0].translatedText))
     .catch(error => {
-      $('#js-google-translate').text(`Error loading this translations`);
+      $('#js-google-translate').text(`Error loading this translation`);
   });
 }
 
@@ -115,11 +145,11 @@ function formatQueryParams(params) {
     return query_items.join('&'); 
 }
 
-function displayYandexTranslate(text) {
-  $('#js-yandex-translate').text(`${text}`); 
+function displayYandexTranslate(yandex) {
+  $('#js-yandex-translate').text(`${yandex}`); 
 }
 
-function displayGoogleTranslate(text) {
+function displayGoogleTranslate(google) {
   $('#js-google-translate').text(`${google}`);
 }
 
@@ -134,24 +164,10 @@ function clearTranslations() {
   });
 }
 
-
 $(handleSubmitText); 
-$(duoCompare);
+$(getSourceCode);
+// $(duoCompare);
 $(clearTranslations);
-
-
-// retrieve translations
-  // convert to an array of words
-  // compare arrays
-// display results
-
-// translate from English and to English 
-
-// https://translate.yandex.net/api/v1.5/tr.json/translate?
-// key=&text=我是你的朋友 &lang=en OR &lang=zh-en
-
-// https://translate.yandex.net/api/v1.5/tr.json/detect?key=&text=我
-
 
 function displaySourceLang(source) {
   const languages = {
